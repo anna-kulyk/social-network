0c48e6d99971923e3ff21741f4a424dbcbaabb3b
Anna <kulyk_anna@outlook.com>
2023-03-28 15:12:59 -0400

    added goBackHandler in NoPage component

--- src/components/NoPage/NoPage.jsx
+++ src/components/NoPage/NoPage.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './NoPage.scss';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const NoPage = () => {
+
+    const navigate = useNavigate();
+    const goBackHandler = () => {  
+        navigate(-1);
+    };
+
     return (
         <div className='nopage _section _page-section'>
             <p className="nopage__error-number">404</p>
             <p className="nopage__error-text">Oops... Page not found!</p>
-            <Link className='nopage__btn _btn' to="/">Go Back</Link>
+            <button className='nopage__btn _btn' onClick={goBackHandler}>Go Back</button>
         </div>
     );
 };
